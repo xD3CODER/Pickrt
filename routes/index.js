7e38dcd49565a5fcbf03de6d71e353c73dff8a77
xD3VHAX <xd3vhax@gmail.com>
2016-12-25 03:18:11 +0100

    Implementing JWT

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,58 @@ const router = express.Router();
 const images = require('../config/images');
 let fs = require('fs');
 const logger = require('../config/logger');
+const User = require('../models/user');
 
 
+const jwt = require('jsonwebtoken');
 
+const passportJWT = require("passport-jwt");
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+const cookieExtractor = function (req) {
+    let token = null;
+    if (req && req.cookies && req.cookies['jwt']) {
+        token = req.cookies['jwt'];
+    }
+    return token;
+};
+
+const jwtOptions = [];
+jwtOptions.jwtFromRequest = ExtractJwt.fromExtractors([ExtractJwt.fromAuthHeader(), cookieExtractor]);
+jwtOptions.secretOrKey = 'secretpass';
+const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+   try {
+       User.findOne({'_id': jwt_payload._id}, function (err, user) {
+           if (err) {
+               logger.debug("Error");
+               return next(err, false);
+           }
+           else if (user) {
+               logger.debug("Found");
+               next(null, user);
+           }
+           else {
+               logger.debug("Not found");
+               return next(null, false);
+           }
+       });
+   }
+   catch (err){
+       logger.debug(err);
+   }
+});
+
+passport.use(strategy);
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   console.info('Current session = ' + req.session.id);
 });
 
-
 router.get('/images', function(req, res, next) {
   res.render('index', { title: 'Express' });
   console.info('Current session = ' + req.session.id);
-
     images.getProfile("base64", images.image, 'user1', function (err, res) {
         if(err) logger.error("Erreurs lors de la création du fichier url : "+err);
         else logger.success("Fichié sauvegardé ! " + res);
@@ -29,10 +67,6 @@ router.get('/images', function(req, res, next) {
   });
   */
 });
-
-
-
-
 
 
 router.get('/vc', function(req, res, next) {
@@ -47,7 +81,7 @@ router.post('/cookie', function(req, res) {
   res.send("old =" +old +"\r" +req.session.cookie.expires);
 });
 
-router.get('/login', function(req, res, next) {
+    router.get('/login', function(req, res, next) {
     res.render('login.ejs', {message: req.flash('loginMessage')});
 
 });
@@ -71,11 +105,82 @@ router.post('/signup', passport.authenticate('local-signup', {
   failureFlash: true,
 }));
 
-router.post('/login', passport.authenticate('local-login', {
-  successRedirect: '/profile',
-  failureRedirect: '/login',
-  failureFlash: true,
-}));
+
+router.post('/login',
+    passport.authenticate('local-login'),
+    function(req, res) {
+        const payload = {};
+        payload._id = req.user._id;
+        logger.debug(payload);
+        const token = jwt.sign(payload , jwtOptions.secretOrKey, { expiresIn: 60*60 , algorithm: 'HS512'});
+        res.cookie('jwt',token, { maxAge: 1000 * 60 * 60 * 24 * 365, httpOnly: true, secure: true });
+        res.json({success: true, token: 'JWT ' + token});
+       //res.redirect('/profile');
+    });
+
+
+
+/*
+router.get('/restricted', passport.authenticate('jwt', { session: false}), function(req, res) {
+
+    logger.log("checking...");
+    const token = getToken(req.headers);
+    if (token) {
+        const decoded = jwt.decode(token, jwtOptions.secretOrKey);
+                res.json({success: true, msg: 'Welcome in the member area !'+ decoded.local.email});
+
+    } else {
+        return res.status(403).send({success: false, msg: 'No token provided.'});
+    }
+})
+*/
+
+
+
+function aaz (req, res, next){
+
+    jwt.verify(cookieExtractor(req), 'secretpass', function(err, decoded) {
+        if (err) {
+            logger.error(err);
+            return next(err);
+        }
+
+    passport.authenticate('jwt', { session: false }, function(err, user, info) {
+        console.log(cookieExtractor(req));
+        if (err) {
+            logger.error(err);
+            return next(err);
+        }
+        if (!user) { return res.send("Redirect to login").end(); }
+        req.user = user;
+        next();
+    })(req, res, next);
+
+    });
+}
+
+
+router.get('/restricted', aaz, function(req, res, next) {
+    logger.success('logged');
+    res.json({success: true, msg: 'Welcome in the member area !'+ req.user});
+});
+
+getToken = function (headers) {
+    if (headers && headers.authorization) {
+        const parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
+
+
+
+
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
