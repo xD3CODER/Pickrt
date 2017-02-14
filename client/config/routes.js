16fa582fb244d1766ac362d1282f8df989d2a3c3
xD3VHAX <xd3vhax@gmail.com>
2017-02-15 00:17:24 +0100

    perf(routes): Changing encrypt/decrypt method for submits
    Remove useless code in routes, improve config file

--- client/config/routes.js
+++ client/config/routes.js
@@ -1,9 +1,8 @@
+const config = require("../../config");
 const express = require("express");
-const passport = require("passport");
 const router = express.Router();
 let fs = require("fs");
 const logger = require("../config/logger");
-var CryptoJS = require("crypto-js");
 const rp = require("request-promise");
 
 
@@ -52,30 +51,16 @@ router.get("/*", function (req, res, next) {
 
 router.get("/", function (req, res, next) {
     res.render("index.ejs", {title: "Express"});
-    console.info("Current session = " + req.session.id);
-
+    logger.log("Current session = " + req.session.id);
 });
-
-router.get("/vc", function (req, res, next) {
-    res.render("index", {title: "Express"});
-});
-
-let cookieName = "p_usr";
-const cookieExtractor = function (req) {
-    let token = null;
-    if (req && req.cookies && req.cookies[cookieName]) {
-        token = req.cookies[cookieName];
-    }
-    return token;
-};
 
 var getUser = function (req, res, next) {
-    logger.debug(req.cookies[cookieName]);
+    logger.debug(req.cookies[config.user_cookie_name]);
     return new Promise(function (done, reject) {
         rp({
             uri: "https://api.loocalhost.tk/connexion",
             headers: {
-                "Authorization": "JWT "+req.cookies[cookieName]
+                "Authorization": "JWT "+req.cookies[config.user_cookie_name]
             },
             json: true
         }).then(function (user) {
@@ -94,12 +79,12 @@ router.get("/profile", getUser, function (req, res) {
 
 
 router.get("/login", function (req, res, next) {
-    if(req.query.setCookie){
-        res.cookie("p_usr", req.query.setCookie, {
+    if(req.query[config.base64.encrypt("setCookie")]){
+        res.cookie(config.user_cookie_name, req.query[config.base64.encrypt("setCookie")], {
             httpOnly: true,
             expires: new Date(Date.now() + 99999999999)
         });
-        res.redirect("https://loocalhost.tk/profile");
+        return res.redirect("/profile");
     }
     res.render("login.ejs");
 });
@@ -117,21 +102,6 @@ router.get("/signup", function (req, res) {
 router.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
-});
-
-
-router.get("/auth/facebook", passport.authenticate("facebook", {scope: ["email", "user_birthday"]}));
-
-router.get("/auth/facebook/callback", passport.authenticate("facebook"), function (req, res) {
-    jwtTokens.createJwt(req, function (token) {
-        res.cookie(jwtTokens.jwtOptions.cookieName, token, {
-            maxAge: 1000 * 60 * 60 * 24 * 365,
-            httpOnly: true,
-            secure: true
-        });
-        res.redirect("/profile");
-    });
-    //res.redirect('/profile');
 });
 
 
--- config.js
+++ config.js
@@ -2,11 +2,24 @@
  * Created by xD3VHAX on 11/02/2017.
  */
 var CryptoJS = require("crypto-js");
+
+
+var base64 = {
+    encrypt : function (data) {
+        return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(data));
+    },
+    decrypt : function (data) {
+        return CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(data));
+    }
+};
+
+
+
 function decryptRequest(req, res, next) {
     let finalReq = {};
     for (var attributename in req.body) {
-        finalReq[CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(attributename))] =
-            CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(req.body[attributename]));
+        finalReq[base64.decrypt(attributename)] =
+            base64.decrypt(req.body[attributename]);
     }
     req.body = finalReq;
     next();
@@ -15,8 +28,8 @@ function decryptRequest(req, res, next) {
 function encryptResponse(array) {
     let finalReq = {};
     for (var attributename in array) {
-        finalReq[CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(attributename))] =
-            CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(array[attributename]));
+        finalReq[base64.encrypt(attributename)] =
+            base64.encrypt(array[attributename]);
     }
     return finalReq;
 }
@@ -34,9 +47,11 @@ let config = {
 		env : "dev",
         url : "api.loocalhost.tk"
 	},
+    user_cookie_name : "p_usr",
 	morgan_level : "dev",
     decryptRequest : decryptRequest,
-    encryptResponse : encryptResponse
+    encryptResponse : encryptResponse,
+    base64 : base64
 };
 
 module.exports = config;
